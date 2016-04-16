6e424e173265bb144bda3739badc35f1f0d0d513
Rickard Andersson <gonz@severnatazvezda.com>
2016-04-16 22:06:14 +0300

    Add item_filter module

---
+++ src/tlrss_item_filter.erl
@@ -0,0 +1,36 @@
+-module(tlrss_item_filter).
+-export([filter/1,
+         filters/0]).
+
+-export([start_link/1,
+         init/1,
+         handle_call/3]).
+
+-behaviour(gen_server).
+-include("records.hrl").
+
+start_link(Filters) ->
+    gen_server:start_link({local, ?MODULE}, ?MODULE, Filters, []).
+
+filter(Items) ->
+    gen_server:call(?MODULE, {filter, Items}).
+
+filters() ->
+    gen_server:call(?MODULE, filters).
+
+init(Filters) ->
+    {ok, Filters}.
+
+handle_call({filter, Items}, _From, Filters) ->
+    Filtered = lists:filter(fun(I) -> wanted_item(I, Filters) end, Items),
+    {reply, {filtered_items, Filtered}, Filters};
+handle_call(filters, _From, Filters) ->
+    {reply, {filters, Filters}, Filters}.
+
+wanted_item(Item, Filters) ->
+    lists:any(fun(Regex) ->
+                      case re:run(Item#item.name, Regex) of
+                          {ok, _} -> true;
+                          _ -> false
+                      end
+              end, Filters).
